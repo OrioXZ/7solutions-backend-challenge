44b8627e03af99fa82c44062617eae33958659ed
OrioXZ <atipong.man@gmail.com>
2026-07-10 19:37:14 +0700

    Connect API to MongoDB

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/OrioXZ/7solutions-backend-challenge/internal/config"
+	"github.com/OrioXZ/7solutions-backend-challenge/internal/database"
 	"github.com/OrioXZ/7solutions-backend-challenge/internal/httpapi"
 )
 
@@ -18,9 +19,18 @@ func main() {
 	cfg := config.Load()
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
+	startupCtx, startupCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	mongoDB, err := database.ConnectMongo(startupCtx, cfg.MongoURI, cfg.MongoDatabase)
+	startupCancel()
+	if err != nil {
+		logger.Error("MongoDB connection failed", "error", err)
+		os.Exit(1)
+	}
+	logger.Info("MongoDB connected", "database", cfg.MongoDatabase)
+
 	server := &http.Server{
 		Addr:              cfg.HTTPAddr,
-		Handler:           httpapi.NewRouter(),
+		Handler:           httpapi.NewRouter(mongoDB),
 		ReadHeaderTimeout: 5 * time.Second,
 	}
 
@@ -43,18 +53,19 @@ func main() {
 	case err := <-serverErr:
 		if err != nil && !errors.Is(err, http.ErrServerClosed) {
 			logger.Error("HTTP server failed", "error", err)
-			os.Exit(1)
 		}
-		return
 	}
 
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownCancel()
 
 	if err := server.Shutdown(shutdownCtx); err != nil {
-		logger.Error("graceful shutdown failed", "error", err)
-		os.Exit(1)
+		logger.Error("HTTP server shutdown failed", "error", err)
 	}
 
-	logger.Info("HTTP server stopped")
+	if err := mongoDB.Close(shutdownCtx); err != nil {
+		logger.Error("MongoDB disconnect failed", "error", err)
+	}
+
+	logger.Info("application stopped")
 }
